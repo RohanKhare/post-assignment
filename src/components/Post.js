8b18295948aa8418e02229137631168c90e22c01
RohanKhare <53505979+RohanKhare@users.noreply.github.com>
2020-06-17 16:04:38 +0530

    Create Post.js

---
+++ src/components/Post.js
@@ -0,0 +1,28 @@
+import React from "react"
+import Title from "./Title"
+import Body from "./Body";
+import Tags from "./Tags";
+
+class Post extends React.Component{
+
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div className='postComponent'>
+                <div>
+                <Title />
+                </div>
+                <div>
+                    <Body />
+                </div>
+                <div>
+                    <Tags />
+                </div>
+            </div>
+        )
+    }
+}
+export default Post
